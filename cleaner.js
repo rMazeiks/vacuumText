5938fd0136d41f40220840d02f72783065234940
ReinisMazeiks <reinis.mazeiks@gmail.com>
2016-11-09 21:51:36 -0500

    More debugging

--- cleaner.js
+++ cleaner.js
@@ -16,11 +16,11 @@ function process() {
         c = c.replace(/\.\.\./g, "…");// combine 3 dots.
     }
     if ($('input#set-punct').is(':checked')) {
-        c = c.replace(/\s*([\.,!?…;])/g, "$1"); // remove spaces before the points
-        c = c.replace(/([\.,!?…;])[\.,!?…]+/g, "$1"); // multiple punctuation mark removal
-        c = c.replace(/\n[\.,!?…;]/g, "\n");  // delete punct. marks after newline
-        c = c.replace(/([\.,!?…;])\s*/g, "$1 ");  // punktuation mark with any spaces to ". " or similar
-        c = c.replace(/[\.!?] ([a-z])/g, " $1");  // delete punct. marks before small characters. But not "..." and "," or ;
+        c = c.replace(/\s*([.,!?…;])/g, "$1"); // remove spaces before the points
+        c = c.replace(/([.,!?…;])[.,!?…;]+/g, "$1"); // multiple punctuation mark removal
+        c = c.replace(/\n[.,!?…;]/g, "\n");  // delete punct. marks after newline
+        c = c.replace(/([.,!?…;])\s*/g, "$1 ");  // punktuation mark with any spaces to ". " or similar
+        c = c.replace(/[.!?] ([a-z])/g, " $1");  // delete punct. marks before small characters. But not "..." and "," or ;
     }
 
     //spaces
@@ -45,8 +45,8 @@ function process() {
         c = c.replace(/ [- –]* /g, "–"); // replace multiple dashes with spaces before and after with one long dash.
     }
     if ($('input#set-quote').is(':checked')) {
-        c = c.replace(/\'\'/g, "\""); // replace two single quotes with a double quote
-        c = c.replace(/[\"\'][\"\' \*]+/g, "\""); // replace [" or '] + multiple quote-like characters with "
+        c = c.replace(/''/g, "\""); // replace two single quotes with a double quote
+        c = c.replace(/["']["' *]+/g, "\""); // replace [" or '] + multiple quote-like characters with "
     }
 
     $("#output")[0].value = c;
